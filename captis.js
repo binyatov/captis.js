ce9dc53e927a526d281acfc0e56afb5036f53786
Pasha Binyatov <pasha@binyatov.com>
2014-08-11 04:12:57 +0200

    Toolbar

--- captis.js
+++ captis.js
@@ -1,3 +1,11 @@
+/**
+* @author Pasha Binyatov <pasha@binyatov.com>
+* @copyright 2014 Pasha Binyatov
+* @license {@link https://github.com/binyatov/captis.js/blob/master/LICENSE|MIT License}
+*/
+
+/**
+*/
 navigator.getUserMedia = (
     navigator.getUserMedia ||
     navigator.webkitGetUserMedia ||
@@ -11,6 +19,37 @@ window.URL = (
     window.mozURL ||
     window.msURL
 );
+
+function initializeToolbar (e) {
+    if (e.ctrlKey && e.keyCode == 69) {
+        document.body.innerHTML += (
+            '<div id="toolbar"> \
+                <i id="camera" class="fa fa-video-camera captis_icon"></i> \
+                <i id="record" class="fa fa-circle"></i> \
+                <i id="screen" class="fa fa-desktop captis_icon"></i> \
+                <i id="save" class="fa fa-save captis_icon"></i> \
+                <i id="edit" class="fa fa-edit captis_icon"></i> \
+                <i id="update" class="fa fa-refresh captis_icon"></i> \
+                <i id="switch" class="fa fa-power-off captis_icon"></i> \
+            </div>'
+        );
+        var close = document.getElementById('switch');
+        document.removeEventListener('keyup', initializeToolbar, false);
+        document.addEventListener('keyup', closeToolbar, false);
+        close.addEventListener('click', closeToolbar, false);
+    }
+}
+
+function closeToolbar (e) {
+    if ((e.ctrlKey && e.keyCode == 69) || e.target.id == 'switch') {
+        var toolbar = document.getElementById('toolbar'),
+            close = document.getElementById('switch');
+        close.removeEventListener('click', closeToolbar, false);
+        toolbar.outerHTML = '';
+        document.removeEventListener('keyup', closeToolbar, false);
+        document.addEventListener('keyup', initializeToolbar, false);
+    }
+}
 
 function mediaStream () {
     if (navigator.getUserMedia) {
@@ -33,3 +72,5 @@ function mediaStream () {
         console.log("getUserMedia not supported");
     }
 }
+
+document.addEventListener('keyup', initializeToolbar, false);
