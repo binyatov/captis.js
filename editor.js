5665b0b1476c026f44ce635f49313ba611104b9f
Pasha Binyatov <pasha@binyatov.com>
2014-11-04 00:44:04 +0100

    Blur

--- editor.js
+++ editor.js
@@ -2,9 +2,10 @@ exports.initializeEditor = function() {
     event.stopPropagation();
     document.getElementById('captis').innerHTML += (
         '<div id="captis_editor"> \
+            <div id="loading"></div> \
             <video id="edit_video" preload></video> \
-            <div id="captis_editor_segments"> \
-            </div> \
+            <div id="captis_editor_segments"></div> \
         </div>'
     );
+    document.body.className += ' blur';
 }
