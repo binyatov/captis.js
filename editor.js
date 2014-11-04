acee1a3cba7e92f57122b51683f81a8a690a36b4
Pasha Binyatov <pasha@binyatov.com>
2014-11-04 01:08:59 +0100

    Progress indicator

--- editor.js
+++ editor.js
@@ -2,10 +2,9 @@ exports.initializeEditor = function() {
     event.stopPropagation();
     document.getElementById('captis').innerHTML += (
         '<div id="captis_editor"> \
-            <div id="loading"></div> \
+            <div id="loading"><progress id="loading_segments" max="10" value="5"></progress></div> \
             <video id="edit_video" preload></video> \
             <div id="captis_editor_segments"></div> \
         </div>'
     );
-    document.body.className += ' blur';
 }
