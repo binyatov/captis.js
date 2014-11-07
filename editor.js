bad703dd9482183e496171738d47c8c9c40b2b78
Pasha Binyatov <pasha@binyatov.com>
2014-11-07 01:17:53 +0100

    Play segment in editor

--- editor.js
+++ editor.js
@@ -15,8 +15,8 @@ var initializeEditor = function() {
         loading = document.getElementById('loading'),
         canvas = document.getElementById('segmentshot'),
         ctx = canvas.getContext('2d');
-        canvas.width = 120;
-        canvas.height = 120;
+        canvas.width = 130;
+        canvas.height = 130;
     parts.innerHTML = '';
     video.src = window.videoURL;
     loader.setAttribute('max', window.segments.length - 1);
@@ -26,30 +26,42 @@ var initializeEditor = function() {
             //console.log(window.segments[i]);
             parts.innerHTML += (
                 '<div class="segment_box" id="segment_'+ i +'"> \
-                    <i class="segment_time" id="time_'+ i +'">00:00:00</i> \
+                    <i class="segment_time">'+ window.formatDuration(window.segments[i].timestamp) +'</i> \
                 </div>'
             );
             video.currentTime = window.segments[i].timestamp;
-            ctx.drawImage(video, 0, 0, 120, 120);
+            ctx.drawImage(video, 0, 0, 130, 130);
             var image = canvas.toDataURL(),
-                box = document.getElementById('segment_' + i),
-                segmentDuration = document.getElementById('time_' + i);
+                box = document.getElementById('segment_' + i);
             //console.log(image);
             box.style.backgroundImage = 'url(' + image + ')';
-            segmentDuration.innerHTML = window.formatDuration(window.segments[i].timestamp);
             loader.value = i;
             i++;
             if (i == window.segments.length) {
                 //parts.style.height = 120 * window.segments.length + 'px';
-                container.style.width = '120px';
+                container.style.width = '130px';
                 loading.style.display = 'none';
                 video.style.display = 'block';
                 video.pause();
                 video.currentTime = 0;
                 clearInterval(loop);
+                var elements = document.getElementsByClassName('segment_box');
+                for (var j = 0; j < elements.length; j++) {
+                    elements[j].addEventListener('click', editorPlaySegment, false);
+                }
+                console.log(elements);
             };
         }, 100);
     };
+
+}
+
+var editorPlaySegment = function () {
+    video = document.getElementById('edit_video');
+    var index = this.id.split('_')[1];
+    video.currentTime = window.segments[index].timestamp;
+    video.muted = false;
+    video.play();
 }
 
 exports.initializeEditor = initializeEditor;
